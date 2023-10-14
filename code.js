f77a60596f34ca87749376c80b530ffd650e96bf
Erik <erik.muradyan.96@gmail.com>
2023-10-14 19:24:39 +0400

    new name

---
+++ code.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    scrollingSpeed: 700,
+    sectionsColor: ['yellowgreen', 'blueviolet', 'red', 'gray', 'pink'],
+    anchors:['firstPage', 'secondPage', 'thirdPage']
+});
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+    // Your custom options
+});
